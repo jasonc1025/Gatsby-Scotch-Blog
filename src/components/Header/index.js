7c94ee835b93ce2ed63e7c9441a20fb4be98acee
jasonc1025 <jasonc@eande.world>
2018-03-26 10:59:03 -0700

    UpdaTe Web Title to Hopefully Work w/ 'Plus'

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -26,7 +26,7 @@ const Header = () => (
             textDecoration: 'none',
           }}
         >
-          The Gray Web Blog
+          The Gray Web Blog Plus
         </Link>
       </h1>
     </div>
